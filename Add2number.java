6d3ae99e35e9063ca7c83551cbddeca9111b3b45
Varsha Sahu <guptavarshajuly15@gmail.com>
2023-05-18 14:12:50 +0530

    refactoring

--- Add2number.java
+++ Add2number.java
@@ -1,19 +1,18 @@
 import java.util.*;
 
-
 public class Add2number {
 
   public static void main(String[] args) {
-    Scanner sc =new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
-    int a = sc .nextInt();
-    int b = sc. nextInt();
+    System.out.println("Enter 1st No:");
+    int a = sc.nextInt();
+    System.out.println("Enter 2nd No:");
+    int b = sc.nextInt();
 
-    {
-      System.out.println(a + b );
-    }
+    System.out.println(a + b);
+
     sc.close();
   }
 
-  
 }
